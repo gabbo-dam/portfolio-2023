9027e05a6cb2037daa4b4eeaaf7aaf6c1a999338
Gabriel Morales <gabbomorales1@gmail.com>
2023-03-09 00:27:39 -0600

    update metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,20 +11,6 @@ export default function Document(props) {
         <link href="/code.svg" rel="icon" sizes="16x16" type="image/png" />
         <link color="#4a9885" href="/code.svg" rel="mask-icon" />
         <meta content="#ffffff" name="theme-color" />
-        <meta content="#ffffff" name="msapplication-TileColor" />
-        <meta
-          content="/static/favicons/browserconfig.xml"
-          name="msapplication-config"
-        />
-        <meta content="14d2e73487fa6c71" name="yandex-verification" />
-        <meta
-          content="eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw"
-          name="google-site-verification"
-        />
-        <meta
-          content="max-snippet:-1, max-image-preview:large, max-video-preview:-1"
-          name="robots"
-        />
       </Head>
       <body className="bg-white dark:bg-black text-white dark:text-black">
         <Main />
